215474a183a5e1a84c853b3afaa91d16d22f3bf2
Machepsilon <erayokutay@hotmail.com>
2022-12-10 00:36:19 +0300

    Navbar Routing

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
 
 function MainNavigation() {
   return (
@@ -15,18 +16,8 @@ function MainNavigation() {
       <h1 className="main-navigation__title">
         <Link to="/">ExpPlaces</Link>
       </h1>
-      <nav className="main-navigation__header-nav">
-        <ul>
-          <li>
-            <Link to="/">Users</Link>
-          </li>
-          <li>
-            <Link to="/">Share Places</Link>
-          </li>
-          <li>
-            <Link to="/">Login</Link>
-          </li>
-        </ul>
+      <nav>
+        <NavLinks />
       </nav>
     </MainHeader>
   );
